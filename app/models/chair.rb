b7a4289180217a8c26b307958677b7c2d1a0b3bc
Sergii <sergykoretsfsp@gmail.com>
2018-08-19 03:00:18 +0300

    not required table

--- app/models/chair.rb
+++ app/models/chair.rb
@@ -1,5 +1,5 @@
 class Chair < ApplicationRecord
   belongs_to :user, required: false
-  belongs_to :table
+  belongs_to :table, required: false
   validates_uniqueness_of :user_id, allow_nil: true
 end
